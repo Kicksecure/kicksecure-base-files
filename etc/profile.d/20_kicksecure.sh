545c69e7d6f5e15fc798abf0da0a174ef8f49034
Patrick Schleizer <adrelanos@riseup.net>
2019-08-13 08:20:13 -0400

    initial implementation (forked from whonix-base-files)

---
+++ etc/profile.d/20_kicksecure.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+## Copyright (C) 2012 - 2018 ENCRYPTED SUPPORT LP <adrelanos@riseup.net>
+## See the file COPYING for copying conditions.
+
+if [ -x /usr/lib/kicksecure/kicksecure.sh ]; then
+   . /usr/lib/kicksecure/kicksecure.sh
+fi
