367274b36b6851a44fa263fa97f54f53e653909d
Patrick Schleizer <adrelanos@whonix.org>
2021-08-04 14:58:22 -0400

    lintian FHS

--- etc/profile.d/20_kicksecure.sh
+++ etc/profile.d/20_kicksecure.sh
@@ -3,6 +3,6 @@
 ## Copyright (C) 2012 - 2021 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-if [ -x /usr/lib/kicksecure/kicksecure.sh ]; then
-   . /usr/lib/kicksecure/kicksecure.sh
+if [ -x /usr/libexec/kicksecure/kicksecure.sh ]; then
+   . /usr/libexec/kicksecure/kicksecure.sh
 fi
